b27c1bcb082e64adf57983d88abd5cfde878eddd
Matteo Padovano <mrbatista@users.noreply.github.com>
2020-05-29 04:14:16 +0200

    fix typo after refactoring

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -5,6 +5,6 @@
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(CapContacts, "CapContacts",
            CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(checkPermissions, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getPermissions, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(getContacts, CAPPluginReturnPromise);
 )
